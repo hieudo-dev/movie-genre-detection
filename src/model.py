19b266ae0b3b86fda90ace538235926ec421b70a
Joel David <jdavidhc1710@gmail.com>
2020-01-06 19:07:31 -0800

    Working Model

--- src/model.py
+++ src/model.py
@@ -5,7 +5,7 @@
 
 
 import keras
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import to_categorical
@@ -15,6 +15,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
+import os
 
 
 # In[ ]:
@@ -22,18 +23,6 @@ from tqdm import tqdm
 
 train = pd.read_csv('Movies_Poster/Multi_Label_dataset/train.csv')
 train.shape[0]
-
-
-# In[ ]:
-
-
-# train_image = []
-# for i in tqdm(range(train.shape[0])):
-#     img = image.load_img('Movies_Poster/Multi_Label_dataset/Images/'+train['Id'][i]+'.jpg',target_size=(400,400,3))
-#     img = image.img_to_array(img)
-#     img = img/255
-#     train_image.append(img)
-# X = np.array(train_image)
 
 
 # In[ ]:
@@ -84,46 +73,48 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
 
 
 # In[ ]:
-def imageLoader(batch_size):
-    L = len(X_train)
-    
-    batch_start = 0
-    batch_end = batch_size
-    while batch_start < L:
-        print(batch_start)
-        train_image = []
-        limit = min(batch_end, L)
+class BatchSequence(keras.utils.Sequence):
+    def __init__(self, x_set, y_set, batch_size):
+        self.x, self.y = x_set, y_set
+        self.batch_size = batch_size
 
-        # print(train_slice.shape)
-        for name in X_train.iloc[batch_start:limit]['Id']:
+    def __len__(self):
+        return int(np.ceil(len(self.x) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        train_image = []
+        for name in self.x.iloc[idx * self.batch_size:(idx + 1) * self.batch_size]['Id']:
             img = image.load_img('Movies_Poster/Multi_Label_dataset/Images/' + name + '.jpg', target_size=(400,400,3))
             img = image.img_to_array(img)
-            # print(img.shape)
             img = img / 255
             train_image.append(img)
-
-        # print(len(train_image))
         
-        batch_X = np.array(train_image)
-        batch_Y = y_train[batch_start:limit]
+        batch_x = np.array(train_image)
+        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
 
-        yield (batch_X, batch_Y)
-
-        batch_start += batch_size   
-        batch_end += batch_size
-
-            
-
-images = imageLoader(500)
-# image = next(images)
-# print(next(images))
-# for index, i in enumerate(images):
-#     print(index)
-    # print(len(i[0]), len(i[1]))
-model.fit_generator(images, steps_per_epoch=14, epochs=10, validation_data=(X_test, y_test), verbose=2, initial_epoch=1)
-# model.fit(image, steps_per_epoch=14, epochs=10, validation_data=(X_test, y_test), verbose=2, initial_epoch=1)
+        return batch_x, batch_y
 
 
+# In[]:
+batch_size = 50
+L = len(X_train)
+batch = BatchSequence(X_train, y_train, batch_size)
+model.fit_generator(batch, epochs=5, validation_data=(X_test, y_test))
 
 
+path = os.getcwd() # Current Path
 
+model.save(os.path.join(path, "dist", "model.h5"))
+print(f"Saved model to disk")
+
+
+# In[]:
+# Load Model
+def loadModel():
+    path = os.getcwd()
+
+    model = load_model(os.path.join(path, "dist", "model.h5"))
+
+    return model
+
+# %%
