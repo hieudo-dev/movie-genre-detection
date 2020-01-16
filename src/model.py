40013e9174eb024995f1c9da1b1bfb00c50217a4
Hieu Do Ngoc <hieudn.uh@gmail.com>
2020-01-16 13:41:58 -0500

    Final ver

--- src/model.py
+++ src/model.py
@@ -57,24 +57,14 @@ X_test = np.array(imgs)
 
 
 model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=(5, 5), activation="relu", input_shape=(400,400,3)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Conv2D(filters=32, kernel_size=(5, 5), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Conv2D(filters=64, kernel_size=(5, 5), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Conv2D(filters=64, kernel_size=(5, 5), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(Conv2D(32, kernel_size=5, strides=2, activation='relu', input_shape=(400,400,3)))
+model.add(Conv2D(64, kernel_size=3, strides=1, activation='relu'))       
+
 model.add(Flatten())
+
 model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(25, activation='sigmoid'))
+model.add(Dense(25, activation='sigmoid'))   # Final Layer using Softmax
+
 
 
 # In[ ]:
